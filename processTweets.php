192fbcbcaf620027c7037dd204dc55345b61eb1d
Reuben Thompson <reuben@alchemysocial.com>
2014-05-07 13:45:30 +0100

    Add summarisation of categories

--- processTweets.php
+++ processTweets.php
@@ -22,6 +22,9 @@ function processItem($item, TwitterRedis $redis) {
 	$words = explode(" ", $text);
 	$result = [];
 	foreach ($words as $word) {
+		if ($word == "") {
+			continue;
+		}
 		print "\tWord is '$word' - will be stemmed as '".stem($word, STEM_ENGLISH)."'\n";
 		if (substr($word, 0, 1 == "@")) {
 			print "\t$word is a username - skipping\n";
@@ -47,4 +50,41 @@ function processItem($item, TwitterRedis $redis) {
 		"scores" => $result
 	];
 	$redis->set("P__".$item['id'], json_encode($toSave), 7200);
+	summarise($redis);
+}
+
+function summarise(TwitterRedis $redis) {
+	$scores = []; $matched = [];
+	$allCategories = $redis->getCategoryList();
+	$tweets = $redis->getKeys("P__*");
+	foreach ($tweets as $key) {
+		$tweet = json_decode($redis->get($key), true);
+		foreach ($tweet['scores'] as $category => $score) {
+			if (!isset($scores[$category])) {
+				$scores[$category] = $score;
+			} else {
+				$scores[$category] += $score;
+			}
+			if ($score == max($tweet['scores'])) {
+				"Assigning tweet to category $category\n";
+				$redis->addToSet("tweets_in_".$category, json_encode($tweet));
+				if (!isset($matched[$category])) {
+					$matched[$category] = 1;
+				} else {
+					$matched[$category]++;
+				}
+			}
+		}
+		foreach ($allCategories as $category) {
+			if (!isset($tweet['scores'][$category]) || $tweet['scores'][$category] !== max($tweet['scores'])) {
+				$redis->addToSet("not_in_".$category, json_encode($tweet));
+			}
+		}
+	}
+	foreach ($scores as $category => $score) {
+		print "For category $category score is $score in {$matched[$category]} matching tweets\n";
+	}
+	$redis->set("MatchedCategories", json_encode($matched), 7200);
+	$redis->set("ScoredCategories", json_encode($scores), 7200);
+	$redis->increment("DataWaiting");
 }
